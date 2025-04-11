91fbf8dd6f2e40f2023560bcbe31323a0214d716
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-04-11 11:45:27 +1000

    ensure theme dir exists when updating

--- monitor/update.fish
+++ monitor/update.fish
@@ -5,6 +5,7 @@ set -q XDG_STATE_HOME && set -l state $XDG_STATE_HOME || set -l state $HOME/.loc
 set -l scheme_path $state/caelestia/scheme/current.txt
 set -l dist $config/spicetify/Themes/caelestia/color.ini
 
+mkdir -p (dirname $dist)
 cp (dirname (status filename))/../template.ini $dist
 cat $scheme_path | while read line
     set -l colour (string split ' ' $line)
